789c70ba78999dff7aec44cb94eec31cd01f970e
DuloT34 <dulo.stvol@gmail.com>
2018-08-31 11:38:15 +0300

    small rewrite

--- jdpython.py
+++ jdpython.py
@@ -1,63 +1,56 @@
-#!/usr/bin/env python
-import requests, bs4, datetime
-
-str1=[]
-str2=[]
-str3=[]
+#!/usr/bin/env python3.6
+import requests
+import sys
+import bs4 as bs
+from datetime import datetime as dt
+from itertools import islice
 
 try:
-    s = requests.get('http://esportlivescore.com/g_dota.html')
+    url = 'http://esportlivescore.com/g_dota.html'
+    s = requests.get(url, timeout=10)
     s.raise_for_status()
 except requests.exceptions.HTTPError as err:
     print(err)
     sys.exit(1)
 
-b=bs4.BeautifulSoup(s.text, "html.parser")
+b = bs.BeautifulSoup(s.text, 'html.parser')
 
-livematches=[]
-try:
-    for tag in b.find("div", {"id": "live"}).find_all("div", {"class": ["match-event event without-score is-live"]}): 
-        timestamp = str(datetime.datetime.fromtimestamp(float((tag.find("span", {"class": ["hide phpunixtime"]})).text[0:10])))
-        gameformat = str(tag.find("div", {"class": ["event-tournament-info"]}).text.strip())
-        teams = str(tag.find("div", {"class": ["team-home"]}).text.strip()) + " vs " + str(tag.find("div", {"class": ["team-away"]}).text.strip())
-        livematches.append(timestamp + '\t' + gameformat + '\t' + teams)
-    str1 = '\n'.join(livematches)
-except:
-    tag = None 
 
-upcomingmatches=[]
-try:
-    for tag in b.find("div", {"id": "upcoming"}).find_all("div", {"class": ["match-event event without-score is-not-live"]}): 
-            timestamp = str(datetime.datetime.fromtimestamp(float((tag.find("span", {"class": ["hide phpunixtime"]})).text[0:10])))
-            gameformat = str(tag.find("div", {"class": ["event-tournament-info"]}).text.strip())
-            teams = str(tag.find("div", {"class": ["team-home"]}).text.strip()) + " vs " + str(tag.find("div", {"class": ["team-away"]}).text.strip())
-            upcomingmatches.append(timestamp + '\t' + gameformat + '\t' + teams)
-    str2 = '\n'.join(upcomingmatches)
-except:
-    tag = None 
+def print_list(columns=0, lst=[]):
+    i, llen = [], len(lst)
+    for n in range(columns):
+        i.append(islice(iter(lst), n, llen, columns))
 
-finishedmatches=[]
-try:
-    for tag in b.find("div", {"id": "finished"}).find_all("div", {"class": ["match-event event with-score is-not-live"]}):
-            timestamp = str(datetime.datetime.fromtimestamp(float((tag.find("span", {"class": ["hide phpunixtime"]})).text[0:10])))
-            score = str(tag.find("span", {"class": ["home-runningscore"]}).text) + ":" + str(tag.find("span", {"class": ["away-runningscore"]}).text)
-            teams = str(tag.find("div", {"class": ["team-home"]}).text.strip()) + " vs " + str(tag.find("div", {"class": ["team-away"]}).text.strip())
-            finishedmatches.append(timestamp + '\t' + score + '\t' + teams)
-    str3 = '\n'.join(finishedmatches)
-except:
-    tag = None
+    for x in zip(*i):
+        print(' '.join(('{}',)*columns).format(*x))
 
-if str1 and str1.strip():
-    print('Live matches:\n' + str1 + '\n')
-else:
-    print('Live matches:\n' + "No live matches" +'\n')
 
-if str2 and str2.strip():
-    print('Upcoming matches:\n' + str2 + '\n')
-else:
-    print('Upcoming matches:\n' + "No upcoming matches" +'\n')
+def get_table(columns=2, tag='', lst=[]):
+    lst.append(dt.fromtimestamp(int(tag['data-unixtime'])))
+    lst.append(tag.select('div.mobile-tournament-info > span')[0].text.strip())
+    if columns == 4:
+        lst.append(':'.join(x for x in
+                            (tag.select('span.home-runningscore')[0].text,
+                             tag.select('span.away-runningscore')[0].text)))
+    lst.append(tag.select('div.event-date a')[0]['title'])
 
-if str3 and str3.strip():
-    print('Finished matches:\n' + str3)
-else:
-    print('Finished matches:\n' + "No recent results" +'\n')
+
+def get_tag(marker='', score='with-score'):
+    live = 'is-not-live'
+    if marker == 'div#upcoming' and score == 'with-score':
+        live = 'is-live'
+    return b.select('{} > div.match-event.event.{}.{}'
+                    .format(marker, live, score))
+
+
+rec = []
+queue = [('div#upcoming', 'with-score', 'Live matches', 4),
+         ('div#upcoming', 'without-score', 'Upcoming matches', 3),
+         ('div#finished', 'with-score', 'Finished matches', 4)]
+
+for marker, score, header, columns in queue:
+    for tag in get_tag(marker, score):
+        get_table(columns, tag, rec)
+    print('\n{}:'.format(header))
+    print_list(columns, rec)
+    rec.clear()
